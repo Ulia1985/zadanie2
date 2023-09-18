aa072faa819ecb3566ac329deece3b01dcdcff2f
Ulia <cs-gorodok@yandex.ru>
2023-09-18 23:10:33 +0300

    change Main

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int number of rubles = 10;// количество рублей для одной бонусной мили
+
+        int ticket1 = 27658; // стоимость первого билета
+        int ticket2 = 34570; // стоимость второго билета
+        int ticket3 = 67000; // стоимость третьего билета
+        int ticket4 = 99999; // стоимость четвертого билета
+        int ticket5 = 128400; // стоимость пятого билета
+
+        System.out.println("Стоимость пяти билетов одной дружной компании:");
+        System.out.println(ticket1 / number of rubles) + " количество бонусных миль за первый билет");
+        System.out.println((ticket2 / number of rubles) + " количество бонусных миль за второй билет");
+        System.out.println((ticket3 / number of rubles) + " количество бонусных миль за третий билет");
+        System.out.println((ticket4 / number of rubles) + " количество бонусных миль за четвертый билет");
+        System.out.println((ticket5 / number of rubles) + " количество бонусных миль за пятый билет");
+
+    }
+}
